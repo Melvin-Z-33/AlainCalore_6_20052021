8bd375685f2c087b95a92bb3b8a178839a233315
Melvin33 <melvin.marley@outlook.fr>
2021-06-02 14:44:13 +0200

    factorisation js

--- page.js
+++ page.js
@@ -5,13 +5,9 @@ const research = async () => {
 	photographers = await fetch(url)
 		.then((res) => res.json())
 		.then((value) => {
-			console.log(value.photographers);
-
-			// extract the id from url
-			const queryString_url_id = window.location.search;
-			const zid = queryString_url_id.slice(1);
-			console.log(zid);
-			const resultat = value.photographers.find((value) => value.id == zid);
+			const extractIdFromUrl = window.location.search;
+			const id = extractIdFromUrl.slice(1);
+			const resultat = value.photographers.find((value) => value.id == id);
 			console.log(resultat);
 		})
 		.catch((err) => console.log('this is the error ' + err));
