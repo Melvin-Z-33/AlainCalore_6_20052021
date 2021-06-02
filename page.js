7883be0b19dd75442c7f920b3450c589bd2bda05
Melvin33 <melvin.marley@outlook.fr>
2021-06-03 01:09:23 +0200

    start css page

--- page.js
+++ page.js
@@ -9,6 +9,23 @@ const research = async () => {
 			const id = extractIdFromUrl.slice(1);
 			const resultat = value.photographers.find((value) => value.id == id);
 			console.log(resultat);
+			const test = document.querySelector('.test');
+			const structureProduit = `
+
+					<div class=card-header>
+						<a href="page.html?${resultat.id}"><img src="/img/Sample_Photos/Photographers_thumbnails/${resultat.portrait}" loading="lazy" alt="" /></a>
+						<h2 class="card-title">${resultat.name} </h2>
+					</div>
+					<div class="card-content">
+						<p class="card-content-city"> ${resultat.city}, ${resultat.country} </p>
+						<p class="card-content-tagline"> ${resultat.tagline} </p>
+						<p class="card-content-price"> ${resultat.price}€/jour </p>
+					</div>
+					<div class="card-content-tag">${resultat.tags}</div>
+				</div>
+`;
+
+			test.innerHTML = structureProduit;
 		})
 		.catch((err) => console.log('this is the error ' + err));
 };
